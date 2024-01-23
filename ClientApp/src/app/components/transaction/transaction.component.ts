03b655993fadbca441e0aea42e1864a33fc0c931
sumon-mobileone <sumon@mobileone.com.my>
2024-01-23 12:24:02 +0600

    UI - Transaction List added

--- ClientApp/src/app/components/transaction/transaction.component.ts
+++ ClientApp/src/app/components/transaction/transaction.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TransactionService } from '../../shared/transaction.service';
+import { SanckbarService } from '../../shared/sanckbar.service';
+import { Transaction } from '../../model/transaction';
+import { Category } from '../../model/category';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-transaction',
@@ -6,5 +13,56 @@ import { Component } from '@angular/core';
   styleUrl: './transaction.component.css'
 })
 export class TransactionComponent {
+
+
+  transactions:Transaction[]=[];
+  categoryList:Category[]=[];
+
+  displayedColumns: string[] = ['title', 'transactionType', 'category', 'amount','creationDate','action'];
+
+  dataSource: MatTableDataSource<Transaction>;
+
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+
+
+  constructor(public tranService: TransactionService, private snackbar: SanckbarService){}
+
+
+
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  this.getAllTransactions();
+}
+
+getAllTransactions(){
+  this.tranService.getAllTransactions().subscribe(
+    res=>{
+      this.transactions = <Transaction[]> res;
+      this. dataSource =  new MatTableDataSource(this.transactions);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      // console.log(res);
+    },
+    err=>{
+      console.log(err);
+    }
+  )
+}
+
+
+
+applyFilter(event: Event) {
+  const filterValue = (event.target as HTMLInputElement).value;
+  this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  if (this.dataSource.paginator) {
+    this.dataSource.paginator.firstPage();
+  }
+}
+
 
 }
--- ClientApp/src/app/shared/transaction.service.ts
+++ ClientApp/src/app/shared/transaction.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
 
-  constructor() { }
+   baseURL = environment.apiURL;
+
+  constructor(private http:HttpClient) { }
+
+
+  getAllTransactions(){
+    return this.http.get(this.baseURL+"Transaction");
+  }
 }
